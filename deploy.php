40456ab680c87a1e81d415ca6f7c7982216f0f01
Andrew Bielecki <ambielecki@gmail.com>
2023-10-01 14:28:43 -0400

    deploy shared

--- deploy.php
+++ deploy.php
@@ -11,7 +11,9 @@ add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
 
-set('bin/npm', function () {
+set('shared_files', ['.env', '.env.local']);
+
+set('which-npm', function () {
     return run('which npm');
 });
 
@@ -21,11 +23,11 @@ task('copy-node', function () {
             run('cp -R {{previous_release}}/node_modules {{release_path}}');
         }
     }
-    run("cd {{release_path}} && {{bin/npm}} install --frozen-lockfile");
+    run("cd {{release_path}} && {{which-npm}} install --frozen-lockfile");
 });
 
 task('npm-build', function () {
-    run("cd {{release_path}} && {{bin/npm}} run build");
+    run("cd {{release_path}} && {{which-npm}} run build");
 });
 
 // Hosts
@@ -34,6 +36,6 @@ host('137.184.202.43')
     ->set('deploy_path', '/var/www/2023-divelogrepeat');
 
 // Hooks
-after('deploy:update_code', 'copy-node');
+after('deploy:writable', 'copy-node');
 after('copy-node', 'npm-build');
 after('deploy:failed', 'deploy:unlock');
